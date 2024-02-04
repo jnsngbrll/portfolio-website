be7db2dea6bb2d550ee2b14081c051f65658cdec
Jansen Gabrillo <Gabrillojansen@gmail.com>
2024-02-04 23:17:36 +0800

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Hero } from '../components/Hero';
-import { Projects } from '../components/Projects';
-
+import { userData } from '../data/userData';
+import { Link } from 'react-router-dom';
+import Aos from 'aos';
+import { FaLongArrowAltRight } from 'react-icons/fa';
 
 export const Home = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+    });
+  }, []);
+
   return (
     <div className="max-w-7xl mx-auto">
       <Hero />
-      <Projects/>
+      <div className="flex flex-col gap-8 py-20">
+        <div className="flex items-center justify-between">
+          <h1 className="text-9xl text-[--secondary] font-extrabold">
+            Projects
+          </h1>
+          <Link
+            to="/projects"
+            onClick={() => window.scrollTo(0, 0)}
+            className="flex items-center gap-2 hover:gap-4 text-[#000000] font-semibold bg-[#ffffff] drop-shadow-md py-2 px-8 rounded"
+          >
+            View All
+            <FaLongArrowAltRight />
+          </Link>
+        </div>
+        {userData.projects.map((project, index) => (
+          <Link
+            data-aos="zoom-in-up"
+            to={project.link}
+            className="relative flex bg-gradient-to-r from-[--accent] to-[--primary] group"
+          >
+            <div className="overflow-hidden">
+              <img
+                src={project.image}
+                alt=""
+                className="w-[500px] transition-all duration-1000 group-hover:scale-125"
+              />
+            </div>
+            <div className="w-[300px] p-8">
+              <h1 className="text-3xl text-[--secondary] font-bold">
+                {project.title}
+              </h1>
+              <p className="text-[--accent]">{project.description}</p>
+            </div>
+            <p className="absolute right-0 bottom-8 text-5xl text-[--accent] font-bold">
+              0{index + 1}
+            </p>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { userData } from '../data/userData';
+import { Link } from 'react-router-dom';
+import Aos from 'aos';
 
 export const Projects = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+    });
+  }, []);
+  
   return (
     <div className="max-w-7xl mx-auto">
       <h1 className="text-9xl text-[--secondary] font-extrabold mt-20">
         Projects
       </h1>
-      <div className="my-10"></div>
+      <div className="my-10 flex flex-col gap-8">
+        {userData.projects.map((project, index) => (
+          <Link
+            data-aos="zoom-in-up"
+            to={project.link}
+            className="relative flex bg-gradient-to-r from-[--accent] to-[--primary] group"
+          >
+            <div className="overflow-hidden">
+              <img
+                src={project.image}
+                alt=""
+                className="w-[500px] transition-all duration-1000 group-hover:scale-125"
+              />
+            </div>
+            <div className="w-[300px] p-8">
+              <h1 className="text-3xl text-[--secondary] font-bold">
+                {project.title}
+              </h1>
+              <p className="text-[--accent]">{project.description}</p>
+            </div>
+            <p className="absolute right-0 bottom-8 text-5xl text-[--accent] font-bold">
+              0{index + 1}
+            </p>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
